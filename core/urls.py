843a1df3e72022e6125adf2665b2d0d5a453a62a
RuslanTopal <baranotik15@gmail.com>
2025-04-14 09:48:26 +0000

    correct all file use flake8

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ from .views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
-    ToggleTaskStatusView
+    ToggleTaskStatusView,
 )
 
 
@@ -17,15 +17,50 @@ app_name = "core"
 
 urlpatterns = [
     # Task urls
-    path('', HomePageView.as_view(), name='home'),
-    path('tasks/create/', TaskCreateView.as_view(), name='task_create'),
-    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
-    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('tasks/<int:pk>/toggle/', ToggleTaskStatusView.as_view(), name='task_toggle'),
-
+    path(
+        "",
+        HomePageView.as_view(),
+        name="home"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task_create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task_update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task_delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        ToggleTaskStatusView.as_view(),
+        name="task_toggle"
+    ),
     # Tag urls
-    path('tags/', TagListView.as_view(), name='tag_list'),
-    path('tags/create/', TagCreateView.as_view(), name='tag_create'),
-    path('tags/<int:pk>/update/', TagUpdateView.as_view(), name='tag_update'),
-    path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tag_delete'),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag_list"
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag_create"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag_update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag_delete"
+    ),
 ]
--- core/views.py
+++ core/views.py
@@ -8,28 +8,28 @@ from .forms import TaskForm
 
 class HomePageView(ListView):
     model = Task
-    template_name = 'core/home.html'
-    context_object_name = 'tasks'
+    template_name = "core/home.html"
+    context_object_name = "tasks"
 
 
 class TaskCreateView(CreateView):
     model = Task
     form_class = TaskForm
-    template_name = 'core/task_form.html'
-    success_url = reverse_lazy('core:home')
+    template_name = "core/task_form.html"
+    success_url = reverse_lazy("core:home")
 
 
 class TaskUpdateView(UpdateView):
     model = Task
     form_class = TaskForm
-    template_name = 'core/task_form.html'
-    success_url = reverse_lazy('core:home')
+    template_name = "core/task_form.html"
+    success_url = reverse_lazy("core:home")
 
 
 class TaskDeleteView(DeleteView):
     model = Task
-    template_name = 'core/task_confirm_delete.html'
-    success_url = reverse_lazy('core:home')
+    template_name = "core/task_confirm_delete.html"
+    success_url = reverse_lazy("core:home")
 
 
 class ToggleTaskStatusView(View):
@@ -37,30 +37,30 @@ class ToggleTaskStatusView(View):
         task = get_object_or_404(Task, pk=pk)
         task.is_done = not task.is_done
         task.save()
-        return redirect('core:home')
+        return redirect("core:home")
 
 
 class TagListView(ListView):
     model = Tag
-    template_name = 'core/tag_list.html'
-    context_object_name = 'tag_list'
+    template_name = "core/tag_list.html"
+    context_object_name = "tag_list"
 
 
 class TagCreateView(CreateView):
     model = Tag
-    fields = ['name']
-    template_name = 'core/tag_form.html'
-    success_url = reverse_lazy('core:tag_list')
+    fields = ["name"]
+    template_name = "core/tag_form.html"
+    success_url = reverse_lazy("core:tag_list")
 
 
 class TagUpdateView(UpdateView):
     model = Tag
-    fields = ['name']
-    template_name = 'core/tag_form.html'
-    success_url = reverse_lazy('core:tag_list')
+    fields = ["name"]
+    template_name = "core/tag_form.html"
+    success_url = reverse_lazy("core:tag_list")
 
 
 class TagDeleteView(DeleteView):
     model = Tag
-    template_name = 'core/tag_confirm_delete.html'
-    success_url = reverse_lazy('core:tag_list')
+    template_name = "core/tag_confirm_delete.html"
+    success_url = reverse_lazy("core:tag_list")

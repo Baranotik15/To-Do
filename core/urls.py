41e7b636912ea9a020bce10434491d47f35791f3
RuslanTopal <baranotik15@gmail.com>
2025-04-14 07:41:27 +0000

    add sidebar

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, TagListView
 
 
 app_name = "core"
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Task, Tag
 
 
 class HomePageView(TemplateView):
     template_name = 'core/home.html'
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = 'core/tag_list.html'
+    context_object_name = 'tag_list'
+
